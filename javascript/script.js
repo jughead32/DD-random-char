f662e05026c2431d6932db906032f2cf0ddc6365
Justin <parbaked@gmail.com>
2020-04-15 17:13:07 -0700

    added JS to fill stats by ID

--- javascript/script.js
+++ javascript/script.js
@@ -2,8 +2,14 @@ let arr = [];
 
 function randomStat() {
     for (i= 0, t=6; i < t; i++){
-    arr.push(Math.floor (Math.random () *16)+3);
+    arr.push(Math.floor (Math.random () * 16)+ 3);
   };
-  return arr;
+
+document.getElementById('str').innerHTML = arr[Math.floor(Math.random()* 6) + 1];
+document.getElementById('dex').innerHTML = arr[Math.floor(Math.random()* 6) + 1];
+document.getElementById('con').innerHTML = arr[Math.floor(Math.random()* 6) + 1];
+document.getElementById('int').innerHTML = arr[Math.floor(Math.random()* 6) + 1];
+document.getElementById('wis').innerHTML = arr[Math.floor(Math.random()* 6) + 1];
+document.getElementById('char').innerHTML = arr[Math.floor(Math.random()* 6) + 1];
+
 };
-randomStat()
